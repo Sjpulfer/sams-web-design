59d9b1b5a9267acaa40820bfa31e96d7a3e9440a
Sjpulfer <sjpulfer18@hotmail.co.uk>
2025-10-15 12:07:42 +0000

    Add fallback and Builder.io init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { BuilderComponent } from '@builder.io/react';
+import { BuilderComponent, builder } from '@builder.io/react';
+
+builder.init('065b75e8053a4a039743d2b93d607518'); // Replace with your actual Builder.io API key
 
 function App() {
   return (
     <div>
+      <h1>If you see this, React is rendering</h1>
       <BuilderComponent model="page" />
     </div>
   );
