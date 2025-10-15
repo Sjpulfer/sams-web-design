6de9a0a4621f148035dd7227743d5debec6a4440
Sjpulfer <sjpulfer18@hotmail.co.uk>
2025-10-15 12:03:56 +0000

    Wire App.tsx to Builder.io content

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BuilderComponent } from '@builder.io/react';
+
+function App() {
+  return (
+    <div>
+      <BuilderComponent model="page" />
+    </div>
+  );
+}
+
+export default App;
